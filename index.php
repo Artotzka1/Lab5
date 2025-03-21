466eb0aa75131c13e9b1e95f6929bd6c02262940
Artotzka1 <157028144+Artotzka1@users.noreply.github.com>
2025-03-21 23:57:59 +0300

    Lab5

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include "db.php";
+$result = mysqli_query($mysql, "SELECT * FROM `terms`");
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Справочник терминов</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h2>Справочник терминов</h2>
+    <div class="table-container">
+        <table>
+            <tr>
+                <th>Термин</th>
+                <th>Определение</th>
+                <th>Изображение</th>
+            </tr>
+            <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['term']) ?></td>
+                    <td><?= htmlspecialchars($row['definition']) ?></td>
+                    <td>
+                        <img src="./<?= htmlspecialchars($row['img']) ?>" title="<?= htmlspecialchars($row['img']) ?>">
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+    </div>
+    <a class="add-link" href="add.php">Добавление нового термина</a>
+</body>
+
+</html>
